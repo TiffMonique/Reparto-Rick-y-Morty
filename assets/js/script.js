9e5efddcf7361f94a43cfeb69008c0e53e2a59c4
Tiffany Monique Matamoros <70666921+TiffMonique@users.noreply.github.com>
2021-12-17 18:44:43 -0600

    Terminado

---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+
+$(document).ready(function () {
+
+
+    $("#form").submit(function (e) {
+        e.preventDefault();
+    });
+
+    var contFila = 1;
+    var contCarta = 1;
+    var contGlobal = 1;
+    var personajes = [];
+    for (let i = 1; i < 43; i++) {
+        $.ajax({
+            type: "GET",
+            url: "https://rickandmortyapi.com/api/character/?page=" + i,
+            dataType: "json",
+            async: true,
+            success: function (data) {
+                for (let j = 0; j < data.results.length; j++) {
+                    if (contCarta > 1) {
+                        contFila++;
+                        contCarta = 6;
+                        agregarFila(contFila);
+                    }
+                    personajes.push(data.results[j].name);
+                    var contenido = document.querySelector("#fila" + contFila);
+                    contenido.innerHTML +=
+                        `
+            <div class="col-sm-3 "></div>
+            <div class="card  align='center'  mb-4  bg-info" style="max-width: 540px; width:540px ">
+            <div class="row no-gutters">
+              <div class="col-md-4 zoom" name="${data.results[j].name
+                            .toString()
+                            .toLowerCase()}" id="carta` +
+                        contGlobal +
+                        `">
+                <img src=${data.results[j].image} class="card-img" alt="...">
+              </div>
+              <div class="col-md-5">
+                <div class="card-body">
+                  <h5 class="card-title">${data.results[j].name}</h5>
+                  <p class="card-text">${data.results[j].status}</p>
+                  <p class="card-text">${data.results[j].species}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-1"></div>
+          `;
+
+                    if (contGlobal == 826) {
+                        for (let i = 1; i < 827; i++) {
+                            $("#carta" + i).zoom();
+                        }
+                    }
+                    contCarta++;
+                    contGlobal++;
+                }
+            },
+        });
+    }
+
+});
+
+function agregarFila(contFila) {
+    var div = document.createElement("div");
+    div.id = "fila" + contFila;
+    div.innerHTML = "";
+    div.className = "row";
+
+    document.body.appendChild(div);
+}
+
+function buscar() {
+    var busqueda = $("#searchBar").val();
+
+    busqueda = busqueda.toString().toLowerCase();
+
+    document
+        .getElementsByName(busqueda)[0]
+        .scrollIntoView({ behavior: "smooth" })
+
+}
+
+function color() {
+    $('.card').css('background-color', 'red');
+
+}
